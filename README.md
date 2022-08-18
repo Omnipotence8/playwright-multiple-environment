5d0884f1c6d46aa38ce29cf4df1578168c3e6a1a
Lyudmyla Kalmykova <omnipotence8.45@gmail.com>
2022-08-18 08:09:22 +0300

    dotenv
    dotenv

---
+++ README.md
@@ -0,0 +1,6 @@
+Reference:
+DotEnv: https://github.com/motdotla/dotenv
+CrossEnv: https://github.com/kentcdodds/cross-env
+
+# install locally (recommended)
+npm install dotenv --save
---
+++ playwright.config.ts
@@ -0,0 +1,12 @@
+import type { PlaywrightTestConfig } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  testMatch: ["tests/test.ts"],
+  timeout: 30000,
+  reporter: "html",
+  use: {
+    headless: false,
+  },
+  globalSetup:"src/utils/globalSetup.ts"
+};
+export default config;
